3fe6d2710bbd31e427b59abc509044d064189176
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-03-05 11:01:04 -0300

    Switched to using direct database queries

--- src/index.js
+++ src/index.js
@@ -3,28 +3,33 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  let get = await fetch("https://posts.api.tabby.page/get/latest/10")
-  const latestPosts = await get.json()
+  const posts = await c.env.DB.prepare(
+    `
+      SELECT *
+      FROM Pages
+      ORDER BY Serial DESC
+      LIMIT 10
+    `
+  ).all()
 
   let rssBody =
 `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">  
-<channel>
-  <title>Tabitha's Blog</title>
-  <link>https://tabby.page</link>
-  <description>The ramblings of a red panda on the internet</description>`
+  <channel>
+    <title>Tabitha's Blog</title>
+    <link>https://tabby.page</link>
+    <description>The ramblings of a red panda on the internet</description>`
   
-  latestPosts.forEach(post => {
+  posts.results.forEach(post => {
     rssBody = rssBody +  `
-  <item>
-    <title>${post.Title}</title>
-    <link>https://tabby.page/posts/${post.Slug}</link>
-  </item>`
+    <item>
+      <title>${post.Title}</title>
+      <link>https://tabby.page/posts/${post.Slug}</link>
+    </item>`
   });
 
   rssBody = rssBody + `
-</channel>
-
+  </channel>
 </rss>`
 
   return c.text(rssBody, 200, {
